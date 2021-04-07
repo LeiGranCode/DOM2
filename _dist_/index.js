cc6b75e27a12eed0c15cd999c3c9fbc2221ca57e
LeiGranCode <LeiGranCode@users.noreply.github.com>
2021-04-07 03:23:26 +0000

    deploy: f8d988cbeea2f625358ae343b27e342fb8ec3795

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,28 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//Crear (1) imagen
+  const minnum=1;
+  const maxnum= 122;
+  const random=()=> Math.floor(Math.random()*(maxnum-minnum))+minnum //random number
+  const createImageNode=()=>{
+  const container= document.createElement('div')
+  container.className="p-4";
+
+  const imagen = document.createElement('img')
+  imagen.className="mx-auto";
+  imagen.width='320'
+  imagen.src=`https://randomfox.ca/images/${random()}.jpg`; //TO DO
+  container.appendChild(imagen)
+  return container;
+}
+//Agregar #imagen
+const nuevaImagen=createImageNode();
+const mountNode=document.getElementById("images");
+const addButton=document.querySelector('button')
+const addImage=()=>{
+  const newImage=createImageNode();
+  mountNode.append(newImage);
+};
+
+addButton.addEventListener("click",addImage)
